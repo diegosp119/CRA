4bac2b398dc9ebe5cc0bcf191da13dd418d97c98
oski214 <oscarnaitor@gmail.com>
2025-02-13 16:04:12 +0100

    Update sudoku.pl

--- Practica1/sudoku.pl
+++ Practica1/sudoku.pl
@@ -1,3 +1,33 @@
-"Alexis deja las gachapons"
+%Hechos
+mujer(maria).
+mujer(sara).
+hombre(carlos).
+hombre(pedro).
+hombre(yo).
+padre(juan, pedro).
+padre(juan, sara).
+padre(juan, yo).
+madre(maria, pedro).
+madre(maria, sara).
 
-"Chavale free to play"
+hijo(pedro, juan).
+hijo(juan, felipe).
+hijo(felipe, victor).
+hijo(victor, alcalde).
+%Reglas
+hermanos(X, Y) :-
+    setof((X, Y), Z^(padre(Z, X), padre(Z, Y), X \= Y), HermanosPadre),
+    setof((X, Y), Z^(madre(Z, X), madre(Z, Y), X \= Y), HermanosMadre),
+    append(HermanosPadre, HermanosMadre, TodosHermanos),
+    list_to_set(TodosHermanos, HermanosUnicos),
+    member((X, Y), HermanosUnicos).
+
+
+%Reglas de recursividad
+descendiente(X, Y) :- hijo(X, Y).
+descendiente(X, Y) :- hijo(X, Z), descendiente(Z, Y).
+% Consultas de ejemplo
+% ?- mujer(maria).
+% ?- mujer(X).
+% ?- padre(juan, pedro).
+% ?- hermanos(X, pedro).
