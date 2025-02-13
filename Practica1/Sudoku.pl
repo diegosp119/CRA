9e72161684b60e9ee3c28f118e3010d890155bc8
Edgar Alexis Conforme Vacacela <edgar.conforme@edu.uah.es>
2025-02-13 16:42:36 +0100

    Update Sudoku.pl

--- Practica1/Sudoku.pl
+++ Practica1/Sudoku.pl
@@ -1,25 +1,31 @@
-%HECHOS
+% Representación del Sudoku como una lista de 81 elementos
+sudoku([., ., 3, ., 2, ., 7, ., .,
+    5, ., ., ., ., 4, ., 3, .,
+    ., ., 3, ., ., ., 2, 5, .,
+    ., 5, ., 1, ., 6, ., ., .,
+    ., 4, 8, ., 7, ., ., ., 2,
+    3, 7, 6, ., 4, 8, ., ., .,
+    8, ., ., ., 2, ., 7, ., 3,
+    ., ., 4, ., ., 2, ., 8, .,
 
-posiciones([1, 2, 3, 4, 5, 6, 7, 8, 9,
-            10, 11, 12, 13, 14, 15, 16, 17, 18, 
-            19, 20, 21, 22, 23, 24, 25, 26, 27, 
-            28, 29, 30, 31, 32, 33, 34, 35, 36, 
-            37, 38, 39, 40, 41, 42, 43, 44, 45, 
-            46, 47, 48, 49, 50, 51, 52, 53, 54, 
-            55, 56, 57, 58, 59, 60, 61, 62, 63, 
-            64, 65, 66, 67, 68, 69, 70, 71, 72, 
-            73, 74, 75, 76, 77, 78, 79, 80, 81]).
+% Predicado para imprimir el tablero de Sudoku
+imprimir_sudoku([]).
+imprimir_sudoku(Tablero) :-
+    imprimir_fila(Tablero, Resto),
+    imprimir_sudoku(Resto).
 
-sudoku([.,.,3,5,8,.,6,2,.,
-        .,6,.,1,7,.,.,8,.,
-        4,.,8,.,6,.,.,.,9,
-        .,.,9,.,4,.,.,.,2,
-        1,4,6,.,.,.,7,3,.,
-        8,3,.,6,.,.,9,4,5,
-        .,2,.,8,5,.,4,.,7,
-        9,5,4,7,3,2,8,.,6,
-        .,8,7,4,.,1,.,.,3]).
+% Predicado para imprimir una fila del tablero
+imprimir_fila(Tablero, Resto) :-
+    tomar(9, Tablero, Fila, Resto),
+    writeln(Fila).
 
-%REGLAS
-
-generar_lista_posibilidades()
+% Predicado para tomar los primeros N elementos de una lista
+tomar(0, L, [], L).
+tomar(N, [H|T], [H|R], Resto) :-
+    N > 0,
+    N1 is N - 1,
+    tomar(N1, T, R, Resto).
+% Predicado principal para ejecutar el programa
+main :-
+    sudoku(Tablero),
+    imprimir_sudoku(Tablero).
