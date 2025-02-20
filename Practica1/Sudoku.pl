98cd9d1e28a470ac85c48f1bd19f4a38f9e50d59
Edgar Alexis Conforme Vacacela <edgar.conforme@edu.uah.es>
2025-02-20 15:23:00 +0100

    20 de febrero

--- Practica1/Sudoku.pl
+++ Practica1/Sudoku.pl
@@ -1,6 +1,5 @@
-<<<<<<< Updated upstream
 % Representación del Sudoku como una lista de 81 elementos
-sudoku([
+sudoku1([
     ., ., 9, 6, ., ., ., 1, .,
     8, ., ., ., ., 1, ., 9, .,
     7, ., ., ., ., ., ., ., 8,
@@ -10,6 +9,19 @@ sudoku([
     ., 8, ., 9, ., ., 5, 4, .,
     6, ., ., 7, 1, ., ., ., 3,
     ., ., 5, ., 8, 4, ., ., 9]).
+
+sudoku([
+    ., 8, ., 5, 7, 6, 2, ., .,
+    ., ., ., 4, ., 2, ., ., .,
+    ., ., ., ., 3, 9, 5, 4, 8,
+    6, 3, ., 9, ., ., 8, 5, 2,
+    ., 9, ., 2, ., ., 3, 7, .,
+    8, ., ., ., 5, ., 6, 9, 4,
+    2, 5, 7, 6, ., 3, 4, 8, 9,
+    3, ., 8, 7, ., ., ., 2, 5,
+    ., 4, ., ., ., ., ., ., 6
+]).
+
 
 % Predicado para imprimir el tablero de Sudoku
 imprimir_sudoku([]).
@@ -40,15 +52,6 @@ numero(7).
 numero(8).
 numero(9).
 
-% Predicado para verificar si una casilla está vacía
-casilla_vacia(Casilla) :-
-    \+ number(Casilla).
-
-% Predicado para obtener las posibilidades para cada casilla vacía
-posibilidades_casilla(Sudoku, Index, Posibilidades) :-
-    nth0(Index, Sudoku, Casilla),
-    casilla_vacia(Casilla),
-    findall(Num, (numero(Num), \+ miembro_fila(Sudoku, Index, Num), \+ miembro_columna(Sudoku, Index, Num), \+ miembro_cuadro(Sudoku, Index, Num)), Posibilidades).
 
 % Predicado para verificar si un número está en la misma fila
 miembro_fila(Sudoku, Index, Num) :-
@@ -78,53 +81,84 @@ miembro_cuadro(Sudoku, Index, Num) :-
     (between(FInicio, FFin, I); between(CInicio, CFin, I)),
     nth0(I, Sudoku, Num).
 
-% Predicado para generar las listas de posibilidades para todas las casillas vacías
+% Predicado para verificar si una casilla está vacía
+casilla_vacia(Casilla) :-
+    \+ number(Casilla).
+
+% Predicado para obtener las posibilidades para cada casilla vacía o generar una lista vacía si la casilla está ocupada
+posibilidades_casilla(Sudoku, Index, Posibilidades) :-
+    nth0(Index, Sudoku, Casilla),
+    (   casilla_vacia(Casilla) -> 
+        findall(Num, (numero(Num), \+ miembro_fila(Sudoku, Index, Num), \+ miembro_columna(Sudoku, Index, Num), \+ miembro_cuadro(Sudoku, Index, Num)), Posibilidades)
+    ;   Posibilidades = []
+    ).
+
+% Predicado para generar las listas de posibilidades para todas las casillas
 generar_posibilidades(Sudoku, Posibilidades) :-
-    findall(P, (nth0(Index, Sudoku, Casilla), casilla_vacia(Casilla), posibilidades_casilla(Sudoku, Index, P)), Posibilidades).
+    generar_posibilidades(Sudoku, 0, Posibilidades).
+
+generar_posibilidades(_, 81, []).
+generar_posibilidades(Sudoku, Index, [Posibilidades|Resto]) :-
+    Index < 81,
+    posibilidades_casilla(Sudoku, Index, Posibilidades),
+    NextIndex is Index + 1,
+    generar_posibilidades(Sudoku, NextIndex, Resto).
 
 % Predicado para imprimir las posibilidades
 imprimir_posibilidades([]).
 imprimir_posibilidades([P|Ps]) :-
     writeln(P),
     imprimir_posibilidades(Ps).
-=======
-%HECHOS
 
-posiciones([1, 2, 3, 4, 5, 6, 7, 8, 9,
-            10, 11, 12, 13, 14, 15, 16, 17, 18, 
-            19, 20, 21, 22, 23, 24, 25, 26, 27, 
-            28, 29, 30, 31, 32, 33, 34, 35, 36, 
-            37, 38, 39, 40, 41, 42, 43, 44, 45, 
-            46, 47, 48, 49, 50, 51, 52, 53, 54, 
-            55, 56, 57, 58, 59, 60, 61, 62, 63, 
-            64, 65, 66, 67, 68, 69, 70, 71, 72, 
-            73, 74, 75, 76, 77, 78, 79, 80, 81]).
 
-sudoku([.,.,3,5,8,.,6,2,.,
-        .,6,.,1,7,.,.,8,.,
-        4,.,8,.,6,.,.,.,9,
-        .,.,9,.,4,.,.,.,2,
-        1,4,6,.,.,.,7,3,.,
-        8,3,.,6,.,.,9,4,5,
-        .,2,.,8,5,.,4,.,7,
-        9,5,4,7,3,2,8,.,6,
-        .,8,7,4,.,1,.,.,3]).
+% Predicado para aplicar la Regla 0 y actualizar el Sudoku
+resolver_regla_0(Sudoku, NuevoSudoku) :-
+    generar_posibilidades(Sudoku, Posibilidades),
+    aplicar_regla_0(Sudoku, Posibilidades, NuevoSudoku).
 
-%REGLAS
+% Predicado para aplicar la Regla 0 una vez
+aplicar_regla_0(Sudoku, Posibilidades, NuevoSudoku) :-
+    actualizar_sudoku(Sudoku, Posibilidades, SudokuActualizado),
+    (   Sudoku \= SudokuActualizado -> 
+        resolver_regla_0(SudokuActualizado, NuevoSudoku)  % Continuar aplicando la Regla 0 si hubo cambios
+    ;   NuevoSudoku = Sudoku  % Si no hubo cambios, el Sudoku está actualizado
+    ).
 
-%Imprimir listas
+% Predicado para actualizar el Sudoku con las posibilidades y aplicar la Regla 0
+actualizar_sudoku([], [], []).
+actualizar_sudoku([C|SudokuResto], [P|PosibilidadesResto], [NuevoC|NuevoSudokuResto]) :-
+    (   length(P, 1) -> 
+        [NuevoC] = P  % Si solo hay una posibilidad, actualizar la casilla con ese número
+    ;   NuevoC = C    % Si no, mantener la casilla original
+    ),
+    actualizar_sudoku(SudokuResto, PosibilidadesResto, NuevoSudokuResto).
 
-imprimir_listas :-
-    posiciones(Posiciones),
-    sudoku(Sudoku),
-    writeln(Posiciones),
-    writeln(Sudoku).
+% Predicado para imprimir el sudoku en forma de lista
+imprimir_sudoku_lista :-
+    sudoku(Tablero),
+    writeln(Tablero).
 
-%Recorrido de listas
+%Predicado para imprimir el sudoku en forma de tablero
+imprimir_sudoku_tablero :-
+    sudoku(Tablero),
+    imprimir_sudoku(Tablero).
 
-%IDEA: Crear una lista con posiciones y casilla intercaladas, despues utilizar variable anónima en caso de ser necesaria.
+%Predicado para generar la lista de posibilidades
+generar_lista_posibilidades :-
+    sudoku(Tablero),
+    generar_posibilidades(Tablero,Posibilidades).
 
-%Generacion de lista
+%Predicado para imprimir lista de posibilidades
+imprimir_lista_posibilidades :-
+    sudoku(Tablero),
+    generar_posibilidades(Tablero,Posibilidades),
+    %writeln(Posibilidades),
+    imprimir_posibilidades(Posibilidades).
 
-generar_lista_posibilidades(Posiciones,Sudoku):-
->>>>>>> Stashed changes
+%Predicado para probar la Regla 0
+probar_regla_0 :-
+    sudoku(Tablero),
+    resolver_regla_0(Tablero, NuevoTablero),
+    imprimir_sudoku(NuevoTablero),
+    generar_posibilidades(NuevoTablero, PosibilidadesActualizadas),
+    imprimir_posibilidades(PosibilidadesActualizadas).
