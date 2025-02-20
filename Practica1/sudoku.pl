588a11429246a3a836c541f9cb7d74aedefd2682
Edgar Alexis Conforme Vacacela <edgar.conforme@edu.uah.es>
2025-02-20 15:21:13 +0100

    hg

--- Practica1/sudoku.pl
+++ Practica1/sudoku.pl
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 % Representación del Sudoku como una lista de 81 elementos
 sudoku([
     ., ., 9, 6, ., ., ., 1, .,
@@ -86,3 +87,44 @@ imprimir_posibilidades([]).
 imprimir_posibilidades([P|Ps]) :-
     writeln(P),
     imprimir_posibilidades(Ps).
+=======
+%HECHOS
+
+posiciones([1, 2, 3, 4, 5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 
+            19, 20, 21, 22, 23, 24, 25, 26, 27, 
+            28, 29, 30, 31, 32, 33, 34, 35, 36, 
+            37, 38, 39, 40, 41, 42, 43, 44, 45, 
+            46, 47, 48, 49, 50, 51, 52, 53, 54, 
+            55, 56, 57, 58, 59, 60, 61, 62, 63, 
+            64, 65, 66, 67, 68, 69, 70, 71, 72, 
+            73, 74, 75, 76, 77, 78, 79, 80, 81]).
+
+sudoku([.,.,3,5,8,.,6,2,.,
+        .,6,.,1,7,.,.,8,.,
+        4,.,8,.,6,.,.,.,9,
+        .,.,9,.,4,.,.,.,2,
+        1,4,6,.,.,.,7,3,.,
+        8,3,.,6,.,.,9,4,5,
+        .,2,.,8,5,.,4,.,7,
+        9,5,4,7,3,2,8,.,6,
+        .,8,7,4,.,1,.,.,3]).
+
+%REGLAS
+
+%Imprimir listas
+
+imprimir_listas :-
+    posiciones(Posiciones),
+    sudoku(Sudoku),
+    writeln(Posiciones),
+    writeln(Sudoku).
+
+%Recorrido de listas
+
+%IDEA: Crear una lista con posiciones y casilla intercaladas, despues utilizar variable anónima en caso de ser necesaria.
+
+%Generacion de lista
+
+generar_lista_posibilidades(Posiciones,Sudoku):-
+>>>>>>> Stashed changes
