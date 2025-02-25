1bfaa5f9c8ded98b1e917067954ccf4d63f82b5d
oski214 <oscarnaitor@gmail.com>
2025-02-25 13:11:59 +0100

    Mejor de nuevo

--- Copia_Seguridad_Regla1.pl
+++ Copia_Seguridad_Regla1.pl
@@ -240,12 +240,11 @@ verificar_repetidos(Filas, Columnas, Cuadrados) :-
     Indice_Cuadrado=[[1,2,3,4,5,6,7,8,9],[10,11,12,13,14,15,16,17,18],[19,20,21,22,23,24,25,26,27],[28,29,30,31,32,33,34,35,36],[37,38,39,40,41,42,43,44,45],[46,47,48,49,50,51,52,53,54],[55,56,57,58,59,60,61,62,63],[64,65,66,67,68,69,70,71,72],[73,74,75,76,77,78,79,80,81]],
 
     % Eliminar repetidos dentro de cada lista de filas, columnas y cuadrados
+    writeln("Indice Filas: "), writeln(Indice_Filas),
     writeln("Fila: "), writeln(Fila),
-    eliminar_repetidos_de_las_listas(Indice_Filas,1, Filas),
-    writeln("Columna: "), writeln(Columna),
-    eliminar_repetidos_de_las_listas(Indice_Columnas, 1,Columnas),
-    writeln("Cuadrado: "), writeln(Cuadrado),
-    eliminar_repetidos_de_las_listas(Indice_Cuadrado, 1, Cuadrados).
+    eliminar_repetidos_de_las_listas(Indice_Filas,1, Filas).
+    %eliminar_repetidos_de_las_listas(Indice_Columnas, 1,Columnas),
+    %eliminar_repetidos_de_las_listas(Indice_Cuadrado, 1, Cuadrado).
 
 
 
